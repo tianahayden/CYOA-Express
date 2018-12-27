f522f8553d059e950721343a9177640ca4d8dea5
Tiana Hayden <tiana.hayden@me.com>
2018-12-27 10:25:49 -0800

    added port option to server file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const app = require('./app');
+const server = require('./app');
 
-app.listen(3000, () => {
-    console.log('Server listening on 3000');
-})
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
